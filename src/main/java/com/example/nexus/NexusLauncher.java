2d5a42163064cd0f7c9f80206d499584fbf8b29d
Aleksei <morgachev_aleksey@mail.ru>
2026-04-07 03:03:02 +0300

    v.1.0 Сформирована первичная структура проекта

---
+++ src/main/java/com/example/nexus/NexusLauncher.java
@@ -0,0 +1,9 @@
+package com.example.nexus;
+
+import com.example.nexus.adapter.TerminalRender;
+
+public class NexusLauncher {
+    public static void main(String[] args) {
+        TerminalRender.main(args);
+    }
+}
---
+++ src/main/java/com/example/nexus/adapter/TerminalRender.java
@@ -0,0 +1,7 @@
+package com.example.nexus.adapter;
+
+public class TerminalRender {
+    public static void main(String[] args) {
+        System.out.println("In jedem Anfang liegt Zauber.");
+    }
+}
